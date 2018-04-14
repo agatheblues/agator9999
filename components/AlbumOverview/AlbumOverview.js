4c8dea82e3e3b2db8e02166b642339612e5df55c
agatheblues <agatheblues@users.noreply.github.com>
2018-04-14 11:38:01 +0200

    Display album image

--- components/AlbumOverview/AlbumOverview.js
+++ components/AlbumOverview/AlbumOverview.js
@@ -40,13 +40,24 @@ class AlbumOverview extends React.Component {
     getAlbum(this.props.id, this.db, this.handleGetAlbumSuccess, this.handleGetAlbumError);
   }
 
+  renderAlbumCover() {
+    let src = '';
+
+    if (!this.state.albumData.hasOwnProperty('images') || this.state.albumData.images.length == 0) {
+      src = '/static/images/missing.jpg';
+    } else {
+      src = this.state.albumData.images[0].url;
+    }
+
+    return <img src={src} alt={'Album Cover'}/>;
+  }
 
   render() {
-    console.log(this.state);
     return (
       <div className='card-container'>
         <div className='card-wrapper'>
           <p>{this.state.albumData.name}</p>
+          {this.renderAlbumCover()}
           <p>{this.state.albumData.added_at}</p>
           <p>{this.state.albumData.source}</p>
           <p>{this.state.albumData.url}</p>
