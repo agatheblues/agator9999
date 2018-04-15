ec72550380a537ce6784606c9453757b3ceea1c6
agatheblues <agatheblues@users.noreply.github.com>
2018-04-15 21:22:10 +0200

    Add button styling

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+require('./Button.scss');
 
 const Button = ({id, label, handleClick}) => (
   <button
     id={id}
-    className="btn btn-primary"
+    className='btn btn-primary'
     onClick={handleClick}
   >{label}</button>
 );
