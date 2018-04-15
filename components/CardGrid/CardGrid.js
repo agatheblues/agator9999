b7f25843e4ff871d7ec2d4ff63c37554a9c0b817
agatheblues <agatheblues@users.noreply.github.com>
2018-04-15 15:41:10 +0200

    Move title class to main.scss

--- components/CardGrid/CardGrid.js
+++ components/CardGrid/CardGrid.js
@@ -38,7 +38,7 @@ class CardGrid extends React.Component {
   render() {
     return (
       <div>
-        <h1 className='cardgrid-title'>Artists</h1>
+        <h1 className='title'>Artists</h1>
         {this.renderCards()}
       </div>
     );
