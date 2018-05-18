7a4874f03ce2af285fcd0d3fb426f2642eb13b0b
agatheblues <agatheblues@users.noreply.github.com>
2018-05-18 21:27:11 +0200

    Remove config files

--- spotify.config.js
+++
@@ -1,7 +0,0 @@
-export const spotifyConfig = {
-  'SPOTIFY_LOGIN_URL': 'https://accounts.spotify.com/authorize',
-  'STATE_KEY': 'spotify_auth_state',
-  'CLIENT_ID': '71f6dc3a88ec45a48f05298731942d35',
-  'REDIRECT_URI': 'http://localhost:8888/#/callback',
-  'SCOPE': 'user-read-private user-read-email user-library-read'
-};
