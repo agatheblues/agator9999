7d7228b18bcb8d98dea1c3ab69f64d58c683a83b
agatheblues <agatheblues@users.noreply.github.com>
2018-05-19 22:00:03 +0200

    Add the Search component to the CardGrid to allow user to filter the artist list

--- components/CardGrid/CardGrid.js
+++ components/CardGrid/CardGrid.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card';
 import Message from '../Message/Message';
+import Search from '../Search/Search';
 import { getAlbums, getArtists } from '../../helpers/FirebaseHelper';
 require('./CardGrid.scss');
 
@@ -12,15 +13,19 @@ class CardGrid extends React.Component {
 
     this.state = {
       artists: [],
+      visibleArtists: [],
       loaded: false,
       error: false,
       albumCount: 0
     };
+
+    this.filterList = this.filterList.bind(this);
   }
 
   handleSuccess(artists) {
     this.setState({
       artists: artists,
+      visibleArtists: artists,
       loaded: true,
       error: false
     });
@@ -35,6 +40,7 @@ class CardGrid extends React.Component {
   handleError() {
     this.setState({
       artists: [],
+      visibleArtists: [],
       loaded: true,
       error: true
     });
@@ -55,7 +61,7 @@ class CardGrid extends React.Component {
     let artists = [];
     data.forEach(function(item) {
       let artist = item.val();
-      artist['id'] = item.key;
+      artist.id = item.key;
       artists.push(artist);
     });
     return artists;
@@ -69,17 +75,27 @@ class CardGrid extends React.Component {
 
   }
 
+  filterList(input) {
+    let visibleArtists = this.state.artists.filter((artist) => {
+      return artist.name.toLowerCase().startsWith(input.toLowerCase());
+    });
+
+    this.setState({
+      visibleArtists: visibleArtists
+    });
+  }
+
   componentDidMount() {
     this.getArtistsList();
     this.getAlbumCount();
   }
 
   renderCards() {
-    if ((this.state.artists.length != 0) && this.state.loaded && !this.state.error) {
+    if ((this.state.artists.length != 0) && this.state.loaded && !this.state.error && (this.state.visibleArtists.length != 0)) {
       return (
         <div className='grid-container'>
           {
-            this.state.artists.map((artist, index) => {
+            this.state.visibleArtists.map((artist, index) => {
               return(
                 <div key={index} >
                   <Card id={artist.id} name={artist.name} imgUrl={artist.imgUrl} totalAlbums={Object.keys(artist.albums).length}/>
@@ -89,6 +105,10 @@ class CardGrid extends React.Component {
           }
         </div>
       );
+    }
+
+    if ((this.state.artists.length != 0) && this.state.loaded && !this.state.error && (this.state.visibleArtists.length == 0)) {
+      return (<p className='cardgrid-message'>No results!</p>);
     }
 
     if ((this.state.artists.length == 0) && this.state.loaded && !this.state.error) {
@@ -111,15 +131,16 @@ class CardGrid extends React.Component {
   render() {
     return (
       <div>
-        <div className='title-container title--marginlr'>
+        <div className='title--marginlr'>
           <h1 className='title'>Artists</h1>
           {this.renderCounts()}
+          <Search filter={this.filterList} />
         </div>
 
         {this.renderCards()}
       </div>
     );
   }
-};
+}
 
 export default CardGrid;
