2d26b7b4cf4935245d2d9d72319d7c9673661a48
agatheblues <agatheblues@users.noreply.github.com>
2018-05-19 21:59:08 +0200

    Add url-loader to webpack config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -33,6 +33,16 @@ module.exports = {
           fallback: 'style-loader',
           use: ['css-loader', 'sass-loader']
         })
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
       }
     ]
   },
