3cf7f12aa8de2a278d7b03f77844c22ea53396b5
agatheblues <agatheblues@users.noreply.github.com>
2018-08-22 22:33:03 +0200

    Add a disabled state class to Button

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 require('./Button.scss');
 
-const Button = ({id, label, handleClick}) => (
-  <button
-    id={id}
-    className='btn'
-    onClick={handleClick}
-  >{label}</button>
-);
+const Button = function({id, label, handleClick, disabled}) {
+
+  const btnClass = classNames({
+    'btn': true,
+    'btn--disabled': !{disabled}
+  });
+
+  return (
+    <button
+      id={id}
+      className={btnClass}
+      onClick={handleClick}
+      disabled={disabled}
+    >{label}</button>
+  );
+};
 
 Button.propTypes = {
   id: PropTypes.string,
   label: PropTypes.string.isRequired,
-  handleClick: PropTypes.func.isRequired
+  handleClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool
 };
 
 export default Button;
