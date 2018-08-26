e60fdeca2abb75a4215b61c19bbb7145ea2d74e8
agatheblues <agatheblues@users.noreply.github.com>
2018-08-26 18:15:23 +0200

    Add ability to link a Spotify album to its Discogs equivalent on creation of the spotify album

--- components/SpotifyCreateAlbum/SpotifyCreateAlbum.js
+++ components/SpotifyCreateAlbum/SpotifyCreateAlbum.js
@@ -4,9 +4,11 @@ import Button from '../Button/Button';
 import SpotifyLogin from '../SpotifyLogin/SpotifyLogin';
 import Message from '../Message/Message';
 import InputText from '../InputText/InputText';
+import Dropdown from '../Dropdown/Dropdown';
 import * as api from '../../helpers/SpotifyHelper';
+import * as dg from '../../helpers/DiscogsHelper';
 import * as fb from '../../helpers/FirebaseHelper';
-import {checkSpotifyUri} from '../../helpers/ErrorHelper';
+import {checkSpotifyUri, checkDiscogsUri} from '../../helpers/ErrorHelper';
 
 class SpotifyCreateAlbum extends React.Component {
   constructor(props) {
@@ -20,13 +22,30 @@ class SpotifyCreateAlbum extends React.Component {
       spotifyUri: '',
       errorSubmit: false,
       errorSpotifyUri: null,
+      discogsUri: '',
+      selectedReleaseType: 'placeholder',
+      errorDiscogsUri: null,
       messageSubmit: null,
       accessToken: this.accessToken
     };
 
+    this.releaseTypeList = [
+      {'id': 'placeholder', 'name': 'Select type', 'hide': true},
+      {'id': 'master', 'name': 'Master'},
+      {'id': 'release', 'name': 'Release'}
+    ];
+
     this.handleErrorSpotifyUri = this.handleErrorSpotifyUri.bind(this);
-    this.handleValue = this.handleValue.bind(this);
+    this.handleErrorDiscogsUri = this.handleErrorDiscogsUri.bind(this);
+    this.handleErrorReleaseType = this.handleErrorReleaseType.bind(this);
+    this.getReleaseType = this.getReleaseType.bind(this);
+    this.handleValueFor = this.handleValueFor.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  getReleaseType(id) {
+    return this.releaseTypeList.filter((s) => (s.id == id))[0].name;
   }
 
   getSpotifyId(s) {
@@ -38,6 +57,27 @@ class SpotifyCreateAlbum extends React.Component {
 
     this.setState({
       errorSpotifyUri: msg
+    });
+
+    return msg;
+  }
+
+
+  handleErrorDiscogsUri(s) {
+    const msg = checkDiscogsUri(s, this.state.selectedReleaseType);
+
+    this.setState({
+      errorDiscogsUri: msg
+    });
+
+    return msg;
+  }
+
+  handleErrorReleaseType(type) {
+    const msg = checkDiscogsUri(this.state.discogsUri, type);
+
+    this.setState({
+      errorDiscogsUri: msg
     });
 
     return msg;
@@ -57,10 +97,15 @@ class SpotifyCreateAlbum extends React.Component {
     });
   }
 
-  handleValue(value) {
-    this.setState({
-      spotifyUri: value
-    });
+  handleValueFor(label) {
+
+    const handleValue = (value) => {
+      this.setState({
+        [label]: value
+      });
+    };
+
+    return handleValue;
   }
 
   getArtistIds(artists) {
@@ -70,7 +115,11 @@ class SpotifyCreateAlbum extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
 
-    if (this.handleErrorSpotifyUri(this.state.spotifyUri)) {
+    const release = this.handleErrorReleaseType(this.state.selectedReleaseType);
+    const discogs = this.handleErrorDiscogsUri(this.state.discogsUri);
+    const spotify = this.handleErrorSpotifyUri(this.state.spotifyUri);
+
+    if (spotify || release || discogs) {
       this.setState({
         errorSubmit: true,
         messageSubmit: 'There are errors in the form!'
@@ -91,6 +140,10 @@ class SpotifyCreateAlbum extends React.Component {
         fb.updateOrSetArtistsFromSingleAlbum(fb.formatArtists(data.artists, fb.formatSpotifyArtist), fb.formatSingleAlbumSummary(data))
           .then(() => api.getArtistsImages(this.accessToken, this.getArtistIds(data.artists)))
       ]))
+      .then(() => dg.getRelease(this.state.discogsUri, this.state.selectedReleaseType))
+      .then(({data}) => {
+        return fb.updateSpotifyAlbumWithDiscogsAlbum(this.getSpotifyId(this.state.spotifyUri), fb.formatDiscogsUpdateAlbum(data));
+      })
       .then(() => this.handleSuccess())
       .catch((error) => this.handleError(error.message));
   }
@@ -111,7 +164,7 @@ class SpotifyCreateAlbum extends React.Component {
                 <InputText
                   handleError={this.handleErrorSpotifyUri}
                   placeholder='Spotify URI of the album as spotify:album:...'
-                  handleValue={this.handleValue}
+                  handleValue={this.handleValueFor('spotifyUri')}
                   value={this.state.spotifyUri}
                 />
               </div>
@@ -119,6 +172,25 @@ class SpotifyCreateAlbum extends React.Component {
               {this.state.errorSpotifyUri &&
                 <Message message={this.state.errorSpotifyUri} error={true} style={'input-msg'} />
               }
+
+              <div className='form-row-container'>
+                <Dropdown
+                  list={this.releaseTypeList}
+                  id={'id'}
+                  value={'name'}
+                  selectedValue={this.getReleaseType(this.state.selectedReleaseType)}
+                  handleSelectedValue={this.handleValueFor('selectedReleaseType')}
+                  handleError={this.handleErrorReleaseType}
+                />
+                <InputText
+                  placeholder={'Discogs URL of album'}
+                  handleValue={this.handleValueFor('discogsUri')}
+                  handleError={this.handleErrorDiscogsUri}
+                  value={this.state.discogsUri}
+                />
+              </div>
+
+              <Message message={this.state.errorDiscogsUri} error={true} style={'input-msg'}/>
 
               {this.state.messageSubmit &&
                 <Message message={this.state.messageSubmit} error={this.state.errorSubmit}/>
