3f853c94f8e48a10c5f63862a6d20c437f3b1cea
agatheblues <agatheblues@users.noreply.github.com>
2018-09-02 18:35:35 +0200

    Create Loading component with spinner

--- components/Loading/Loading.js
+++ components/Loading/Loading.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+require('./Loading.scss');
 
-const Loading = function() {
+const Loading = function({fullPage, label}) {
+
+  const loadingClass = classNames({
+    'loading-container': true,
+    'loading-container--full': fullPage
+  });
 
   return (
-    <div className='loading-container'>
-      <p>Loading...</p>
+    <div className={loadingClass}>
+      <div className='spinner'>
+        <div className='rect1'></div>
+        <div className='rect2'></div>
+        <div className='rect3'></div>
+        <div className='rect4'></div>
+        <div className='rect5'></div>
+        {label &&
+          <p className='note'>{label}</p>
+        }
+      </div>
     </div>
   );
 };
+
+Loading.propTypes = {
+  fullPage: PropTypes.bool.isRequired,
+  label: PropTypes.string
+};
+
 
 
 export default Loading;
