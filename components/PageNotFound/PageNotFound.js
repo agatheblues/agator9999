6169465c0fe5a61bab225c86302cbfdc4746c61f
agatheblues <agatheblues@users.noreply.github.com>
2018-09-02 10:31:02 +0200

    Add a PageNotFound component to show when route does not exists

---
+++ components/PageNotFound/PageNotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+require('./PageNotFound.scss');
+
+const PageNotFound = function() {
+  return (
+    <div className='content-container'>
+
+      <div className='back-to-library'>
+        <Link to='/'>&#9839; Back to library</Link>
+      </div>
+      <h1>Oops! This page does not exists.</h1>
+    </div>
+  );
+};
+
+
+export default PageNotFound;
