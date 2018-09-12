1a15188e535ebb03dc272b02041e6a8377eb3f3c
agatheblues <agatheblues@users.noreply.github.com>
2018-09-12 23:06:12 +0200

    Move express to dev dependencies. Only import firebase needed modules

--- Helpers/FirebaseHelper.js
+++ Helpers/FirebaseHelper.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 import { firebaseConfig } from '../config';
 
 /******* FORMATTING *******/
