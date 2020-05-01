62e77da10059b40559a1190ee14700a906af6558
Agathe L <lenclen.agathe@gmail.com>
2020-05-01 14:32:35 +0200

    Fix PageNotFound present on all pages

--- main.js
+++ main.js
@@ -81,40 +81,36 @@ class App extends React.Component {
 
   renderPage(Page, adminPage = true) {
     return (props) => (
-      <UserContext.Consumer>
-        {({ user, admin, logout }) => {
-          if (adminPage && !admin) return <Redirect to="/404" />;
-          return <Page {...props} user={user} admin={admin} logout={logout} />;
-        }}
-      </UserContext.Consumer>
+      <UserContext.Provider value={this.state}>
+        <UserContext.Consumer>
+          {({ user, admin, logout }) => {
+            if (adminPage && !admin) return <Redirect to="/404" />;
+            return <Page {...props} user={user} admin={admin} logout={logout} />;
+          }}
+        </UserContext.Consumer>
+      </UserContext.Provider>
     )
   }
 
   render() {
     if (!this.state.loaded) return <Loading fullPage={true} label={'Loading...'} />;
+    if (!this.state.user) return <AuthPage login={this.login} />;
 
     return (
       <HashRouter>
         <Switch>
-          {!this.state.user &&
-            <Route render={() => <AuthPage login={this.login} />} />
-          }
-          {this.state.user &&
-            <UserContext.Provider value={this.state}>
-              <Route exact path="/" render={this.renderPage(HomePage, false)} />
-              <Route exact path="/spotify/sync" render={this.renderPage(SpotifySyncPage)} />
-              <Route exact path="/spotify/login" render={this.renderPage(SpotifyLogin)} />
-              <Route path="/:access_token(access_token=.*)" render={this.renderPage(SpotifyLogin)} />
-              <Route exact path="/album/create" render={this.renderPage(CreateAlbumPage)} />
-              <Route exact path="/artist/:id" render={this.renderPage(ArtistPage, false)} />
-              <Route exact path="/artist/:id/merge" render={this.renderPage(ArtistMergePage)} />
-              <Route exact path="/artist/:id/unmerge" render={this.renderPage(ArtistUnmergePage)} />
-              <Route exact path="/404" component={PageNotFound} />
-              <Route component={PageNotFound} />
-            </UserContext.Provider>
-          }
+          <Route exact path="/" render={this.renderPage(HomePage, false)} />
+          <Route exact path="/spotify/sync" render={this.renderPage(SpotifySyncPage)} />
+          <Route exact path="/spotify/login" render={this.renderPage(SpotifyLogin)} />
+          <Route path="/:access_token(access_token=.*)" render={this.renderPage(SpotifyLogin)} />
+          <Route exact path="/album/create" render={this.renderPage(CreateAlbumPage)} />
+          <Route exact path="/artist/:id" render={this.renderPage(ArtistPage, false)} />
+          <Route exact path="/artist/:id/merge" render={this.renderPage(ArtistMergePage)} />
+          <Route exact path="/artist/:id/unmerge" render={this.renderPage(ArtistUnmergePage)} />
+          <Route exact path="/404" component={PageNotFound} />
+          <Route path="*" component={PageNotFound} />
         </Switch>
-      </HashRouter>
+      </HashRouter >
     );
   }
 }
